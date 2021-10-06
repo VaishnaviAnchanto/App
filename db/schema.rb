ccf0fade343bdc507ee13e489826f29eac839776
Vaishnavi Rathi <vaishnavi.rathi@anchanto.com>
2021-10-06 15:43:04 +0530

    add

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_04_071935) do
+ActiveRecord::Schema.define(version: 2021_10_06_100049) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "upated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
