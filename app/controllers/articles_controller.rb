fa7fe9d4870e6e80f7d5bf1095c3484000a04c95
Vaishnavi Rathi <vaishnavi.rathi@anchanto.com>
2021-10-08 00:49:44 +0530

    pagination

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,9 @@ class ArticlesController < ApplicationController
     end
 
     def index
-        @articles = Article.all
+        @articles = Article.paginate(page: params[:page], per_page: 5)
+
+
     end
 
     def new
@@ -21,6 +23,7 @@ class ArticlesController < ApplicationController
     def create
         
         @article = Article.new(article_params)
+        @article.user = User.first
         if @article.save
             flash[:notice] = "Article was created successfully."
             redirect_to @article
